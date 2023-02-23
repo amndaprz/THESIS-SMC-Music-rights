a3ab54083bd6ecabd8aaa1cda52a488f6bac3273
tim-dc <timothy_james_delacruz@dlsu.edu.ph>
2023-02-23 23:02:43 +0800

    Added onlyArtist modifier

--- contracts/Artist.sol
+++ contracts/Artist.sol
@@ -13,6 +13,20 @@ contract Artist {
         ArtistOwner = msg.sender;
     }
 
+    modifier onlyArtist() {
+        require(msg.sender == ArtistOwner, "Only the Artist can perform this action");
+
+        _;
+    }
+
+    function addSong(string memory songName, uint256 _price) public onlyArtist view returns(string memory){
+        return string(abi.encodePacked(songName, "has been added to your library, priced at", _price));
+    }
+
+    function sellSong() public onlyArtist view returns (string memory){
+        return "Song has been listed on the market";
+    }
+
     function signMRC(string memory contractName, uint256 _a) public returns(string memory) {
         emit signContract(msg.sender);
         return string(abi.encodePacked(contractName, _a));
@@ -25,4 +39,6 @@ contract Artist {
     function getAddress() public view returns (address){
         return address(this);
     }
+
+    
 }
