e625d94ea223dfeb98724e58a8bcabb5b6863b6e
tim-dc <timothy_james_delacruz@dlsu.edu.ph>
2023-03-12 01:15:36 +0800

    Added Access Control Roles and checkers

--- contracts/MusicRightsToken.sol
+++ contracts/MusicRightsToken.sol
@@ -9,23 +9,43 @@ import "@openzeppelin/contracts@4.8.2/utils/Counters.sol";
 
 contract MusicRightsToken is ERC721, ERC721Enumerable, Pausable, AccessControl {
 
-    // 1. Property Variables
+    // 1. Property Variables ------------------------------------------------------------------------------------
     using Counters for Counters.Counter;
 
     bytes32 public constant PAUSER_ROLE = keccak256("PAUSER_ROLE");
     bytes32 public constant MINTER_ROLE = keccak256("MINTER_ROLE");
+    bytes32 public constant CLIENT_ROLE = keccak256("CLIENT_ROLE");
+    bytes32 public constant LABEL_ROLE = keccak256("LABEL_ROLE");
+    bytes32 public constant ARTIST_ROLE = keccak256("ARTIST_ROLE");
+
+    // Counter of how many tokens have been minted
     Counters.Counter private _tokenIdCounter;
 
+    // Token price and supply cap
     uint256 public MINT_PRICE = 0.005 ether;
     uint256 public MAX_SUPPLY = 10000;
 
 
-    // 2. Lifecycle Methods
-
+    // 2. Lifecycle Methods ------------------------------------------------------------------------------------
     constructor() ERC721("MusicRights Token", "MRT") {
-        _grantRole(DEFAULT_ADMIN_ROLE, msg.sender);
+        // -------- Restrict these to certain accounts only (ADMIN)----------------
+        // _grantRole(DEFAULT_ADMIN_ROLE, msg.sender);
+        _grantRole(DEFAULT_ADMIN_ROLE, 0x5B38Da6a701c568545dCfcB03FcB875f56beddC4);
+
+   
+        // ------- Client Accounts ------
+        _grantRole(CLIENT_ROLE, msg.sender);
+
+        // ------- Label Accounts ------
+        _grantRole(LABEL_ROLE, msg.sender);
+
+        // ------- Artist Accounts ------
+        _grantRole(ARTIST_ROLE, msg.sender);
+
+        // ------- Pauser Accounts ------
         _grantRole(PAUSER_ROLE, msg.sender);
         _grantRole(MINTER_ROLE, msg.sender);
+        
         //  Start token ID incrementor
         _tokenIdCounter.increment();
     }
@@ -35,7 +55,8 @@ contract MusicRightsToken is ERC721, ERC721Enumerable, Pausable, AccessControl {
     //     payable(owner()).transfer(address(this).balance);
     // }
 
-    // 3. Pausable Functions
+
+    // 3. Pausable Functions ------------------------------------------------------------------------------------
 
     function pause() public onlyRole(PAUSER_ROLE) {
         _pause();
@@ -45,7 +66,7 @@ contract MusicRightsToken is ERC721, ERC721Enumerable, Pausable, AccessControl {
         _unpause();
     }
 
-    // 4. Minting Functions
+    // 4. Minting Functions ------------------------------------------------------------------------------------
 
     // Remove onlyRole() if we want all roles to get a token for music rights contract
 
@@ -55,7 +76,7 @@ contract MusicRightsToken is ERC721, ERC721Enumerable, Pausable, AccessControl {
         _safeMint(to, tokenId);
     }
 
-    // 5. Other Functions
+    // 5. Other Functions ------------------------------------------------------------------------------------
 
     function _beforeTokenTransfer(address from, address to, uint256 tokenId, uint256 batchSize)
         internal
@@ -63,6 +84,28 @@ contract MusicRightsToken is ERC721, ERC721Enumerable, Pausable, AccessControl {
         override(ERC721, ERC721Enumerable)
     {
         super._beforeTokenTransfer(from, to, tokenId, batchSize);
+    }
+
+    // 6. Checking Functions ------------------------------------------------------------------------------------
+
+    function _isAdmin () public view returns (bool) {
+        require(hasRole(DEFAULT_ADMIN_ROLE, msg.sender), "User is not an ADMIN");
+        return true;
+    }
+
+    function _isClient () public view returns (bool) {
+        require(hasRole(CLIENT_ROLE, msg.sender), "User is not a CLIENT");
+        return true;
+    }
+
+    function _isLabel () public view returns (bool) {
+        require(hasRole(LABEL_ROLE, msg.sender), "User is not a CLIENT");
+        return true;
+    }
+
+    function _isArtist () public view returns (bool) {
+        require(hasRole(ARTIST_ROLE, msg.sender), "User is not a CLIENT");
+        return true;
     }
 
     // The following functions are overrides required by Solidity.
@@ -75,4 +118,16 @@ contract MusicRightsToken is ERC721, ERC721Enumerable, Pausable, AccessControl {
     {
         return super.supportsInterface(interfaceId);
     }
+
+
 }
+
+/*
+
+Acc 1: 
+0x5B38Da6a701c568545dCfcB03FcB875f56beddC4
+
+Acc 2:
+0xAb8483F64d9C6d1EcF9b849Ae677dD3315835cb2
+
+*/
