b20678b66bb808a29483bd0e23b83ea4e4ebed28
inaevangelista <ina_evangelista@dlsu.edu.ph>
2023-03-23 00:22:02 +0800

    Restrict pages role-based

--- dapp/src/App.js
+++ dapp/src/App.js
@@ -12,16 +12,20 @@ import "./css/Homepage.css"
 import "./css/Login.css"
 import "./css/Notif.css"
 
-
-
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 
 document.body.style.background = "#232226";
 function App() {
+
+  var role = "client";
+  console.log(role);
+
   return (
     <div className>
       <BrowserRouter>
         <Routes>
+          
+          {/*
           <Route exact path="/"element={<Client />}/>
           <Route path="/Test"element={<Test />}/>
           <Route path="/Artist"element={<Artist />}/>
@@ -30,6 +34,32 @@ function App() {
           <Route path="/Stream"element={<Stream />}/>
           <Route path="/ModalTest"element={<ModalTest />}/>
           <Route path="/NotifTest"element={<NotifTest />}/>
+          */}
+          
+          {role === "client" ? (
+          <Route exact path="/"element={<Client />}/>
+          ) : (
+          console.log("not client") // or navigate somewhere
+          )}
+
+          {role === "artist" ? (
+          <Route path="/Artist"element={<Artist />}/>
+          ) : (
+          console.log("not artist")
+          )}  
+
+          {role === "label" ? (
+          <Route path="/Label"element={<Label />}/>
+          ) : (
+          console.log("not label")
+          )}
+
+          <Route path="/Test"element={<Test />}/>
+          <Route path="/Dev"element={<Dev />}/>
+          <Route path="/Stream"element={<Stream />}/>
+          <Route path="/ModalTest"element={<ModalTest />}/>
+          <Route path="/NotifTest"element={<NotifTest />}/>
+
         </Routes>
       </BrowserRouter>
   </div>
