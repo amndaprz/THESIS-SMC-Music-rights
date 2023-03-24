a178d1239187059f2e5a776d2121b02bbfb576ae
inaevangelista <ina_evangelista@dlsu.edu.ph>
2023-03-25 00:56:19 +0800

    Added Home page

--- dapp/src/App.js
+++ dapp/src/App.js
@@ -1,4 +1,5 @@
 import Test from './pages/Test';
+import Home from './pages/Home';
 import Client from './pages/Client';
 import Artist from './pages/Artist';
 import Label from './pages/Label';
@@ -12,20 +13,34 @@ import "./css/Homepage.css"
 import "./css/Login.css"
 import "./css/Notif.css"
 
-
-
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 
 document.body.style.background = "#232226";
 function App() {
+  var role = "client";
   return (
     <div className>
       <BrowserRouter>
         <Routes>
-          <Route exact path="/"element={<Client />}/>
-          <Route path="/Test"element={<Test />}/>
+          <Route exact path="/"element={<Home />}/>
+          {role === "client" ? (
+          <Route exact path="/Client"element={<Client />}/>
+          ) : (
+          console.log("not client") // or navigate somewhere
+          )}
+
+          {role === "artist" ? (
           <Route path="/Artist"element={<Artist />}/>
+          ) : (
+          console.log("not artist")
+          )}  
+
+          {role === "label" ? (
           <Route path="/Label"element={<Label />}/>
+          ) : (
+          console.log("not label")
+          )}
+          <Route path="/Test"element={<Test />}/>
           <Route path="/Dev"element={<Dev />}/>
           <Route path="/Stream"element={<Stream />}/>
           <Route path="/ModalTest"element={<ModalTest />}/>
