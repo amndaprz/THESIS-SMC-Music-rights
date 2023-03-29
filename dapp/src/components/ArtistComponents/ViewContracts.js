2ad58e93258b30f96cc575dcba9888bd833627cb
inaevangelista <ina_evangelista@dlsu.edu.ph>
2023-03-30 03:54:31 +0800

    Fixed views & Added content loader

--- dapp/src/components/ArtistComponents/ViewContracts.js
+++ dapp/src/components/ArtistComponents/ViewContracts.js
@@ -3,7 +3,7 @@ import CardContract from '../Cards/CardContract'
 function ViewContracts(){
 
     return(
-        <div class="row py-4 px-5 card-deck">
+        <div class="row py-4 px-0 card-deck">
             <CardContract/>
             <CardContract/>
             <CardContract/>
