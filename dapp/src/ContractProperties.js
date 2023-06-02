a945691336de6bf0d0616486199b94985678362d
nicscheng <82209083+nicscheng@users.noreply.github.com>
2023-06-02 22:12:51 +0800

    changes

--- dapp/src/ContractProperties.js
+++ dapp/src/ContractProperties.js
@@ -1,8 +1,8 @@
 import ERC721 from './erc721ABI.json';
 import Web3 from "web3";
 
-export const contractAddress = "0x3A34f12eD4Cd5C1DFDe46614FB636CBD7CEeDFCd";
+export const contractAddress = "0x53cF1C4F84469f8fA3d5Af20e0eA6852dB49fb68";
 
 export const contractABI = ERC721;
-export const web3 = new Web3(Web3.givenProvider || "http://127.0.0.1:7545");
+const web3 = new Web3(new Web3.providers.HttpProvider('https://sepolia.infura.io/v3/92a80845fcca49f0aa359bb806201101'));
 export const contract = new web3.eth.Contract(contractABI, contractAddress);
