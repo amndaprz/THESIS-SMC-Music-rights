18d32287aea2aa61629ffbf92e3c5d3fe0d151bb
Nicole Cheng <nicole_t_cheng@dlsu.edu.ph>
2023-06-04 03:45:16 +0800

    gitignore

--- dapp/src/ContractProperties.js
+++ dapp/src/ContractProperties.js
@@ -3,7 +3,7 @@ import Web3 from "web3";
 //require("dotenv").config();
 
 export const contractAddress = "0xC88A62772a89Ff40e8F9F9e06EaC910c28B7a966";
-const http_key = process.env.ALCHEMY_ENDPOINT;
+const http_key = process.env.REACT_APP_ALCHEMY_KEY;
 
 export const contractABI = ERC721;
 export const web3 = new Web3(http_key);
