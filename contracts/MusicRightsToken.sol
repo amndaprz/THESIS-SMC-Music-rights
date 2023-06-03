726fd51f8e5829daf12352bbe05bef0cdda309a5
Nicole Cheng <nicole_t_cheng@dlsu.edu.ph>
2023-06-04 03:30:28 +0800

    fixed polyfill error

--- contracts/MusicRightsToken.sol
+++ contracts/MusicRightsToken.sol
@@ -1,7 +1,6 @@
 // SPDX-License-Identifier: DLSU--TINA
 pragma solidity 0.8.4;
 
-
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Enumerable.sol";
 import "@openzeppelin/contracts/security/Pausable.sol";
--- dapp/src/ContractProperties.js
+++ dapp/src/ContractProperties.js
@@ -1,28 +1,10 @@
 import ERC721 from './erc721ABI.json';
 import Web3 from "web3";
+//require("dotenv").config();
 
 export const contractAddress = "0xC88A62772a89Ff40e8F9F9e06EaC910c28B7a966";
+const http_key = process.env.ALCHEMY_ENDPOINT;
 
-const fs = require("fs")
-const path = require("path")
-
-const getTheAbi = () => {
-  try {
-    const dir = path.resolve(
-      __dirname,
-      "./artifacts/contracts/MusicRightsToken.sol/MusicRightsToken.json"
-    )
-    const file = fs.readFileSync(dir, "utf8")
-    const json = JSON.parse(file)
-    const abi = json.abi
-    console.log(`abi`, abi)
-
-    return abi
-  } catch (e) {
-    console.log(`e`, e)
-  }
-}
-
-export const contractABI = getTheAbi();
-const web3 = new Web3(new Web3.providers.HttpProvider('https://eth-goerli.g.alchemy.com/v2/7xQu8LjQYqTTF-gWhwKGbHfEdxF5-WCN'));
+export const contractABI = ERC721;
+export const web3 = new Web3(http_key);
 export const contract = new web3.eth.Contract(contractABI, contractAddress);
