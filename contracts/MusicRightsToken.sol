da4a14891aa7b339c696cf4d22baf1057e067493
amndaprz <amanda_perez@dlsu.edu.ph>
2023-06-07 18:27:27 +0800

    Sign Artist MRT

--- contracts/MusicRightsToken.sol
+++ contracts/MusicRightsToken.sol
@@ -5,7 +5,6 @@ pragma solidity >=0.5.17;
 import "@openzeppelin/contracts@4.8.2/token/ERC721/ERC721.sol";
 import "@openzeppelin/contracts@4.8.2/token/ERC721/extensions/ERC721Enumerable.sol";
 import "@openzeppelin/contracts@4.8.2/security/Pausable.sol";
-import "@openzeppelin/contracts@4.8.2/utils/Counters.sol";
 import "./RoleAccess.sol";
 
 
@@ -16,13 +15,6 @@ contract MusicRightsToken is ERC721, ERC721Enumerable, Pausable{
     // 1. Property Variables ------------------------------------------------------------------------------------
    
 
-    // Dump Wallet
-    address payable public fixedWallet = payable(0xf9677b7CD5fdDf10697eb4D3976784c55e671F9C);
-
-    // Counter of how many tokens have been minted
-    Counters.Counter private _tokenIdCounter;
-
-
     // Token price and supply cap
     uint256 public MINT_PRICE = 0.005 ether;
     uint256 public MAX_SUPPLY = 10000;
@@ -30,93 +22,57 @@ contract MusicRightsToken is ERC721, ERC721Enumerable, Pausable{
     struct MRC {
         uint256 token_id;
         string ipfsHash;
+        uint256 status; //0- not minted, 1 - minted
     }
+
+     //Role Access SMC Instance
+    RoleAccess roleAccessInstance;
 	
 	
 	 
     //IPFS hash
      uint256 public tokenIdCounter;
 	 MRC[] public _MRCs; 
-    //Owner of the token & token counter
-    // mapping (uint256 => address) private _Owner;
-
-
-    //Role Access SMC Instance
-    RoleAccess roleAccessInstance;
-
 
 
     // 2. Lifecycle Methods ------------------------------------------------------------------------------------
     constructor() ERC721("MusicRights Token", "MRT") {
-        // // -------- Restrict these to certain accounts only (ADMIN)----------------
-        // // _grantRole(DEFAULT_ADMIN_ROLE, msg.sender);
-        // _grantRole(DEFAULT_ADMIN_ROLE, 0xd9f50E7e3f141E9b8dDccB23d58D5f959e7D3FE7);
-   
-        // // ------- Client Accounts ------
-        // _grantRole(CLIENT_ROLE, 0x81bD0B9d5D5F3D4d19e98806CeCeE28911d99daa);
-
-
-        // // ------- Label Accounts ------
-        // _grantRole(LABEL_ROLE, 0xd9f50E7e3f141E9b8dDccB23d58D5f959e7D3FE7);
-
-
-        // // ------- Artist Accounts ------
-        // _grantRole(ARTIST_ROLE, 0x81bD0B9d5D5F3D4d19e98806CeCeE28911d99daa);
-
-
-        // ------- Pauser Accounts ------
-        // _grantRole(PAUSER_ROLE, msg.sender);
-        // _grantRole(MINTER_ROLE, msg.sender);
-       
+      
         //  Start token ID incrementor
         tokenIdCounter = 0;
 
-        //Constructor for role access smc
-		// Change address to deployed RoleAccess smc
-        roleAccessInstance = RoleAccess(0xDA0bab807633f07f013f94DD0E6A4F96F8742B53);
+        //Put contract address ng role access here once implement na ung modifeirs
+        // roleAccessInstance = RoleAccess();
+
     }
 
+    // 3. Minting Functions ------------------------------------------------------------------------------------
 
-    // 3. Pausable Functions ------------------------------------------------------------------------------------
-
-
-    // function pause() public onlyRole(PAUSER_ROLE) {
-    //     _pause();
-    // }
-
-
-    // function unpause() public onlyRole(PAUSER_ROLE) {
-    //     _unpause();
-    // }
-
-
-    // 4. Minting Functions ------------------------------------------------------------------------------------
-    //  WITH IPFS
-    function safeMintWithHash(string calldata ipfsHash) public {
-        uint256 newTokenCounter = tokenIdCounter + 1;
-
-        tokenIdCounter = tokenIdCounter + 1;
-
+    //Call when label inputs form
+    function nonMint(string calldata ipfsHash) public  {
+         uint256 newTokenCounter = tokenIdCounter + 1;
 
         if(newTokenCounter < MAX_SUPPLY){
-            _safeMint(msg.sender, newTokenCounter);
-            _MRCs.push(MRC(newTokenCounter, ipfsHash));
-            _Owner[newTokenCounter] = msg.sender;
+            _MRCs.push(MRC(newTokenCounter, ipfsHash, 0));
+        }
+    }
+
+    // Call when artist signs
+    function safeMintWithToken(uint256 token_id) public {
+
+        //Check if id exists
+        uint256 i = tokenIdCounter;
+
+        for (uint x =0; x < i; x++){
+            if(_MRCs[x].token_id == token_id)
+                if(token_id < MAX_SUPPLY){
+                    _MRCs[x].status = 1;//change status to minted
+                    _safeMint(msg.sender, token_id);
+                }
+                
         }
        
     }
-
-    // Dump Wallet payment
-
-    // event payDumpWallet(address dumpWallet, address from, uint256 amount);
-
-    // function payToDumpWallet(address payable dumpWallet, address from, uint256 amount) payable external{
-        
-    //     require(msg.value >= amount, "Insufficient balance");
-    //     dumpWallet.transfer(amount);
-
-    //     emit payDumpWallet(dumpWallet, from, amount);
-    // }
 
 
     // Dump Wallet Withdraw
@@ -129,33 +85,10 @@ contract MusicRightsToken is ERC721, ERC721Enumerable, Pausable{
     }
 
 
-    // // WITHOUT IPFS
-    //   function safeMintBase() public returns (uint256) {
-    //     _tokenIdCounter.increment();
-    //     uint256 newTokenCounter = _tokenIdCounter.current();
-
-
-    //     if(newTokenCounter <= MAX_SUPPLY){
-    //         _safeMint(msg.sender, newTokenCounter);
-    //         // _IPFSHashes[newTokenCounter] = ipfsHash;
-    //         _Owner[newTokenCounter] = msg.sender;
-    //         return newTokenCounter;
-    //     }else{
-    //         _tokenIdCounter.decrement();
-    //         return newTokenCounter;
-    //     }
-
-    // }
     // 5. Other Functions ------------------------------------------------------------------------------------
 
 
-    //Event: Transfer token and request payment from client
-
-    // event transferWithPayment (address label, address client, uint256 token_id, uint256 total_fee);
-
-    //Transfer token & request payment
-    //Total fee from ipfs
-
+  
     function transferETH(address payable sender, address payable recipientAddress, uint amount) public {
         address payable recipient = recipientAddress;
         recipient.transfer(amount);
@@ -175,7 +108,6 @@ contract MusicRightsToken is ERC721, ERC721Enumerable, Pausable{
 
         safeTransferFrom(label, client, token_id);
         
-        // emit transferWithPayment(label,client,token_id, total_fee);
 
     }
 
@@ -183,6 +115,7 @@ contract MusicRightsToken is ERC721, ERC721Enumerable, Pausable{
      function getAllMRCs() public view returns (MRC[] memory){
         return _MRCs;
     }
+    
 
 
     function getMRC(uint256 token_id) public view returns (string memory) {
@@ -195,6 +128,8 @@ contract MusicRightsToken is ERC721, ERC721Enumerable, Pausable{
         
         return temp;
     }
+
+
 
     function _beforeTokenTransfer(address from, address to, uint256 tokenId, uint256 batchSize)
         internal
