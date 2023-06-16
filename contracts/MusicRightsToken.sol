82dd9493e0c74cd2fbbaac3f4aa3b49885490d7a
amndaprz <amanda_perez@dlsu.edu.ph>
2023-06-17 06:22:27 +0800

    Cleaning .sol files

--- contracts/MusicRightsToken.sol
+++ contracts/MusicRightsToken.sol
@@ -13,32 +13,29 @@ contract MusicRightsToken is ERC721, ERC721Enumerable, Pausable{
     uint256 public MAX_SUPPLY = 10000;
     
     struct MRC {
-        uint256 token_id;
+        uint256 tokenId;
         string ipfsHash;
         uint256 status;//0-undefined, 1 - pending, 2-listed, 3-bought, 4 - declined
         string client; //"" - no client 
     }
 
-    //IPFS hash
      uint256 public tokenIdCounter;
-    //  MRC[] public _MRCs; //listed
     mapping(uint256 => MRC) internal _MRC;
     uint256[] public Tokens;
 
-
-    // 2. Lifecycle Methods ------------------------------------------------------------------------------------
     constructor() ERC721("MusicRights Token", "MRT") {
         //  Start token ID incrementor
         tokenIdCounter = 0;
     }
 
-    /*
+/*
     nonMint Function
-   
-    Variables:
+    This function is called when a Label user inputs information for a new Commercial contract.
+    
+    Parameters:
         ipfsHash        | STRING input for ipfs hash where supplementary information 
                           about the contract is stored
-    */
+*/
 
     function nonMint(string calldata ipfsHash) public  {
          uint256 newTokenCounter = tokenIdCounter + 1;
@@ -47,81 +44,140 @@ contract MusicRightsToken is ERC721, ERC721Enumerable, Pausable{
             
             _MRC[newTokenCounter] = MRC(newTokenCounter,ipfsHash,1,"");
             tokenIdCounter = newTokenCounter;
-            Tokens.push(tokenIDCounter);
+            Tokens.push(tokenIdCounter);
         }
     }
 
-    /*
+/*
     safeMintWithToken Function
-   
-    Variables:
+    Called when an Artist signs a commercial contract proposed by their Label
+    
+    Parameters:
         tokenId         | INT input for the corresponding tokenID representing the index of 
-    */
+                          the MRT item they are trying to mint
+*/
     function safeMintWithToken(uint256 tokenId) public {
 
-        _MRC[token_id].status = 2;
-        _safeMint(msg.sender, token_id);
+        _MRC[tokenId].status = 2;
+        _safeMint(msg.sender, tokenId);
     }
 
     
-    // Call when artist rejects total fee offer
-    function rejectProposal(uint256 token_id) public {
-        _MRC[token_id].status = 4;
+/*
+    rejectProposal Function
+    Called when an Artist declines a commercial contract proposed by their Label
+    
+    Parameters:
+        tokenId         | INT input for the corresponding tokenID representing the index of 
+                          the MRT item they are trying to reject
+*/
+    function rejectProposal(uint256 tokenId) public {
+        _MRC[tokenId].status = 4;
     }
 
 
     // Dump Wallet Withdraw
-    event withdrawDumpWallet(address recipient, uint256 amount);
+    // event withdrawDumpWallet(address recipient, uint256 amount);
 
-    function withdrawFromDumpWallet(address payable recipient, uint256 amount) payable external {
-        recipient.transfer(msg.value);
-        emit withdrawDumpWallet(recipient, amount);
-    }
+    // function withdrawFromDumpWallet(address payable recipient, uint256 amount) payable external {
+    //     recipient.transfer(msg.value);
+    //     emit withdrawDumpWallet(recipient, amount);
+    // }
 
-    // 5. Other Functions ------------------------------------------------------------------------------------  
+
+    
+/*
+    transferETH Function
+    Called when an Artist declines a commercial contract proposed by their Label
+    
+    Parameters:
+        tokenId         | INT input for the corresponding tokenID representing the index of 
+                          the MRT item they are trying to reject
+*/
     function transferETH(address payable recipientAddress, uint amount) public {
         address payable recipient = recipientAddress;
         recipient.transfer(amount);
     }
 
-    function transferBuyout(string calldata client, uint256 token_id, uint256 total_fee, uint256 percent_label, uint256 percent_artist, address payable artist, address payable label)payable public {
-        //For demo purposes, sender does not need to be the owner. This will be implented during THS-ST3
-        require(_exists(token_id), "Token ID does not exist");
-        // require(msg.value == total_fee, "Amount of ethers sent does not match the total fee");
+/*
+    transferBuyout Function
+    Called when an Artist declines a commercial contract proposed by their Label
+    
+    Parameters:
+        tokenId         | UINT256 input for the token id of the corresponding MRC contract that the client wants to buy
+        total_fee        | UINT256 input for the total fee to be paid by the client (msg.sender), as requested by the label and artist
+        percent_label    | UINT256 input for the peercentage that the Label is entitled to
+        percent_artist   | UINT256 input for the percetnage that the Artist is entitles to
+        artist           | ADDRESS PAYABLE input for the address of the Artist wallet/account
+        label            | ADDRESS PAYABLE input for the address of the Label wallent/account
 
+*/
+
+    function transferBuyout(uint256 tokenId, uint256 total_fee, uint256 percent_label, uint256 percent_artist, address payable artist, address payable label)payable public {
+        //For demo purposes, sender does not need to be the owner. This will be implented during THS-ST3
+        require(_exists(tokenId), "Token ID does not exist");
         
         // Split total_fee
         transferETH (artist, total_fee*(percent_artist/100));
         transferETH (label, total_fee*(percent_label/100));
-        _MRC[token_id].status = 3;
-
-        safeTransferFrom(label, msg.sender , token_id); //double check if client address to pls thanks
+        _MRC[tokenId].status = 3;
+        //transfers the token from the Artist to the Client
+        safeTransferFrom(label, msg.sender , tokenId);
     }
 
-    // function getAllMRCs() public view returns (MRC[] memory){
-    //     return _MRCs;
-    // }
 
-    function getStatus(uint256 token_id) public view returns (uint256){
+/*
+    getStatus Function
+    Called when an Artist declines a commercial contract proposed by their Label
+    
+    Parameters:
+        tokenId         | UINT256 input for the token id corresponding to the index of the MRC that is being queried
+*/
+
+    function getStatus(uint256 tokenId) public view returns (uint256){
         
-        return _MRC[token_id].status;
-
-
+        return _MRC[tokenId].status;
     }
 
+/*
+    getTokenLength Function
+    Returns the number of tokens stored in the smart contract as metadata
+    
+    Returns: 
+*/
     function getTokenLength() public view returns (uint256){
         return tokenIdCounter;
 
     }
 
+
+/*
+    getTokens Function
+    Returns the array of token ids that are stoed in the smart contract
+    
+    Returns: 
+        uint256[]         | An array of uint256 that corresponds to the array of token ids
+
+*/
     function getTokens() public view returns (uint256[] memory){
         return Tokens;
     }
 
+
+/*
+    getMRC Function
+    Returns the array of token ids that are stoed in the smart contract
     
-    function getMRC(uint256 token_id) public view returns (MRC memory) {
+    Parameters:
+        tokenId         | UINT256 input for the token id corresponding to the index of the MRC that is being queried
+   
+    Returns:  
+        MRC              | MRC struct that corresponds to the token id input
+
+*/   
+    function getMRC(uint256 tokenId) public view returns (MRC memory) {
         
-        return _MRC[token_id];
+        return _MRC[tokenId];
     }
 
     function _beforeTokenTransfer(address from, address to, uint256 tokenId, uint256 batchSize)
--- contracts/Stream.sol
+++ contracts/Stream.sol
@@ -8,10 +8,11 @@ contract StreamContract {
         uint256 prevStream;
         uint256 currStream;
         string ipfsHash;
-        uint256 stream_id;
+        uint256 streamId;
     }
     // Stores all stream struct
     mapping(uint256 => Stream) streams;
+    uint256[] streamID;
 
 
    
@@ -20,19 +21,33 @@ contract StreamContract {
         uint256 prevStream = 0;
         uint256 currStream = 0;
         streams[ _streamCounter] = Stream(prevStream, currStream, ipfsHash, _streamCounter);
-
+        streamID.push(_streamCounter);
 
         return  _streamCounter;
     }
 
-    function simulateStreams (uint256 n_streams, uint256 stream_id) public return (uint256){
+
+
+/*
+    simulateStreams Function
+    Handles the streaming processes. It adds the number of streams that the admin adds to each streaming contract.
+    If the number of prevStreams is 10 streams lesser than the current stream, then the function returns the number of streams that must be paid for
+    Parameters:
+        nStreams                | UINT256 input for the number of streams to be added to the currStreams parameter of the streaming contract
+        streamId                | UINT256 input for the id corresponding to the index of the STREAM struct that is being queried
+   
+    Returns:  
+        uint256                 | number of streams that has to be paid for
+
+*/  
+    function simulateStreams (uint256 nStreams, uint256 streamId) public returns (uint256){
        
 
-        Stream storage temp_stream = streams [stream_id];
+        Stream storage temp_stream = streams [streamId];
 
 
         temp_stream.prevStream = temp_stream.currStream;
-        temp_stream.currStream = temp_stream.currStream + n_streams;
+        temp_stream.currStream = temp_stream.currStream + nStreams;
         
         uint256 update = 0 ; //default , 0
         if (  temp_stream.currStream - temp_stream.prevStream >= 10) {
@@ -45,23 +60,36 @@ contract StreamContract {
 
     }
 
+/*
+    getStream Function
+    Returns the alias of the user corresponding to the roleAddress input
+    
+    Parameters:
+        id                | UINT256 input for the id corresponding to the index of the STREAM struct that is being queried
+   
+    Returns:  
+        uint256           | number of streams that has been paid for
+        uint256           | total number of streams that are being stored into the smart contract
+        string            | string ipfs hash that corresponds to the streaming contract
+        uint256           | int value corresponding to he id of the streaming contract
 
+*/  
     function getStream(uint256 id) public view returns (uint256,uint256, string memory, uint256) {
         Stream memory stream = streams[id];
-        return (stream.prevStream, stream.currStream, stream.ipfsHash, stream.stream_id);
+        return (stream.prevStream, stream.currStream, stream.ipfsHash, stream.streamId);
     }
 
 
+/*
+    getAllStreams Function
+    Returns the array of addresses that are stoed in the smart contract
+    
+    Returns: 
+        uint256[]         | An array of stream structs that corresponds to the array of streaming ids stored in the smart contract
 
-    function getAllStreams() public view returns (Stream[] memory){
-
-
-        Stream[] memory streamArray ;
-        for (uint x =0; x < _streamCounter; x++){
-            streamArray[x] = streams[x];
-        }
-
-        return streamArray;
+*/
+    function getAllStreams() public view returns (uint256[] memory){
+        return streamID;
     }
 
 
