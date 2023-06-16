b1b0f2f10b04a524424af61ea2d0993d2c52a84a
nicscheng <82209083+nicscheng@users.noreply.github.com>
2023-06-17 02:14:04 +0800

    commercial contract fixed

--- dapp/src/components/ArtistComponents/CommercialProposals.js
+++ dapp/src/components/ArtistComponents/CommercialProposals.js
@@ -7,6 +7,7 @@ import CardProposal from '../Cards/CardCommProposal';
 
 import {contractAddress, contractABI, web3, contract, contract_RA} from '../../ContractProperties';
 
+let account;
 
 function ViewContractProposals(){
 
@@ -52,6 +53,13 @@ function ViewContractProposals(){
 
     const displayAllInfo = async() => {
         let IPFS = await ipfsClient();
+
+        const accounts = await web3.eth.requestAccounts();
+        account = accounts[0];
+        // get alias of current user with address
+        console.log("ACCOUNT: " + typeof account);
+        let alias = await contract_RA.methods.getAlias(account).call();
+
         let info = [];
         const data =[];
         const temp_data = [];
@@ -88,75 +96,43 @@ function ViewContractProposals(){
                     for await (const chunk of IPFS.cat(hash)) {
                         console.log(chunk);
                         data.push(chunk); 
-                                
-                                // temp_data.push(JSON.parse(Buffer.concat(chunk).toString()));
-            
+                                            
                         info = Buffer.concat(data).toString();
                         console.log("INFO - " + info);
             
                         try {
                             const data = JSON.parse(info);
                             console.log(data);
-                            temp_data.push(data);
-                            //setJsonObj(temp_data);
+                            if(alias === data.artist_name)
+                            {
+                                console.log("IS ARTIST: " + data.artist_name);
+                                temp_data.push(data);
+                            }                            
                             } catch (error) {
                             const position = parseInt(error.message.split(' ').pop(), 10);
                             const cleanJsonString = info.substring(0, position);
                             const data = JSON.parse(cleanJsonString);
                             console.log(temp_data);
                             temp_data.push(data);
-                            //setJsonObj(temp_data);
                             
                             }
                             
                             data.pop();
                     }
 
-
                 }
             }
             
             
-            console.log("Extraction successful!");
-
-            //console.log(allResults.size());
-            // if for loop doesnt work use while loop instead
-            // for await (const chunk of IPFS.cat(cid)) {
-            //   data.push(chunk);
-            // }
-            
-            
+            console.log("Extraction successful!");       
         
         } catch (err) {
             console.error("Error while retrieving data from IPFS:", err); // handle any errors
         }
         console.log("temp_data datatype: " + data);
         setJsonObj(temp_data);
-        //setJsonObj(data);
         console.log("TEMP_DATA" + typeof(temp_data));
         console.log(Buffer.concat(data).toString());
-         // log the contents of the file to the console
-        
-        
-        // info = Buffer.concat(data).toString();
-        // console.log("INFO - " + info);
-        //info = "["+info.åreplace(/\n/g, ",")+"]";
-        // const jsonObj = 0;
-        // try {
-        //     const jsonObj = JSON.parse(info);
-        // } catch (err) { console.error("Error parse-ing: ", err);}
-
-        // dataObject = jsonObj;
-
-        // console.log("Parsed: " + dataObject);
-        
-        // const jsonObj = JSON.parse(info);
-        // console.log(typeof data);
-        //getDisplaySongs();
-
-        
-    
-
     
     }
     
