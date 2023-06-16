077b4d5dd1a96eb2ef0f136c31613fbd3cd60f3a
nicscheng <82209083+nicscheng@users.noreply.github.com>
2023-06-16 20:37:54 +0800

    delete pages

--- dapp/src/App.js
+++ dapp/src/App.js
@@ -1,13 +1,8 @@
-import Test from './pages/Test';
 import Home from './pages/Home';
 import Client from './pages/Client';
 import Artist from './pages/Artist';
 import Label from './pages/Label';
-import Dev from './pages/Dev';
 import Stream from './pages/Stream';
-import ModalTest from './pages/ModalTest';
-import NotifTest from './pages/NotifTest';
-import ListTest from './pages/ListTest';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import "./css/App.css"
 import "./css/Homepage.css"
@@ -28,12 +23,7 @@ function App() {
           <Route exact path="/Client"element={<Client />}/>
           <Route path="/Artist"element={<Artist />}/>
           <Route path="/Label"element={<Label />}/>
-          <Route path="/Test"element={<Test />}/>
-          <Route path="/Dev"element={<Dev />}/>
           <Route path="/Stream"element={<Stream />}/>
-          <Route path="/ModalTest"element={<ModalTest />}/>
-          <Route path="/NotifTest"element={<NotifTest />}/>
-          <Route path="/ListTest"element={<ListTest />}/>
         </Routes>
       </BrowserRouter>
   </div>
