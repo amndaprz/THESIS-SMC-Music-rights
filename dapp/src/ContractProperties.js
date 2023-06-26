3ef2a69ccab192f9d0e7bdb7fc0215bf1945fd4f
nicscheng <82209083+nicscheng@users.noreply.github.com>
2023-06-26 13:46:47 +0800

    change

--- dapp/src/ContractProperties.js
+++ dapp/src/ContractProperties.js
@@ -6,7 +6,7 @@ import Web3 from "web3";
 // import dotenv from "dotenv";
 
 // MusicRightsToken.sol -------------------------
-export const contractAddress = "0x8C36BF68C6994EA9B5c95E2f2E9DF6F4720d316d";
+export const contractAddress = "0x78F139Df914b009aC43317D3E12bdCD831Cf0E0d";
 
 export const contractABI = ERC721;
 export const web3 = new Web3(Web3.givenProvider || "http://127.0.0.1:7545");
@@ -15,7 +15,7 @@ export const contract = new web3.eth.Contract(contractABI, contractAddress);
 
 
 // RoleAccess.sol ------------------
-export const contractAddress_RA = "0x6A2945e802d4f83f3Be3C1670C3faeae9462B98b";
+export const contractAddress_RA = "0xF83fCfffcCC994f061668C28fBDA64ef81DCa5C9";
 
 export const contractABI_RA = ERC721_RA;
 export const web3_RA = new Web3(Web3.givenProvider || "http://127.0.0.1:7545");
@@ -24,7 +24,7 @@ export const contract_RA = new web3_RA.eth.Contract(contractABI_RA, contractAddr
 
 
 // Stream.sol ------------------
-export const contractAddress_Stream = "0x43b93917EC3e5b0b16f2EA2E38e6571f949D8c11";
+export const contractAddress_Stream = "0xFD28656b5D908d5c95d181004277Dd4dca4A830B";
 
 export const contractABI_Stream = ERC721_Stream;
 export const web3_Stream = new Web3(Web3.givenProvider || "http://127.0.0.1:7545");
